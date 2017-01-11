559365b6786635f1cf68840bd42fc92779fe8860
Bruno Filho <brgccf@cin.ufpe.br>
2017-01-11 13:24:10 -0300

    Adicionando Classes basicas de Laboratorios

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111131617) do
+ActiveRecord::Schema.define(version: 20170111161945) do
 
   create_table "centers", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "centros", force: :cascade do |t|
-    t.string   "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170111131617) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["center_id"], name: "index_departments_on_center_id"
+  end
+
+  create_table "laboratories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_laboratories_on_user_id"
   end
 
 end
