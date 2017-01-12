141f5e3a53d85f31681afb54ae5dac9d02499828
Rafael Albuquerque <rafaellsma@gmail.com>
2017-01-12 15:02:00 -0300

    Devise configurado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
   resources :laboratories
   resources :departments
   resources :centers
-  root to: "centers#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
