99d1aba829ff2eb976e93fb7ba73fea6c7a77de9
Bruno Filho <brgccf@cin.ufpe.br>
2017-03-17 17:55:32 -0300

    Primeiros passos com o controlador de User. - problema de compilação no controlador.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    
+    @user.admin = true
+    @user.approved = true
+    @user.save
+
   end
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
