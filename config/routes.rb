b28309688a5872ede4c69a67c378a77cceeac683
Bruno Filho <brgccf@cin.ufpe.br>
2017-03-17 17:34:37 -0300

    Primeiro passo com cadastro de usuários

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   resources :centers
   root to: "home#index"
 
+  authenticated :user, lambda { |u| u.admin? } do
+    get 'reg-admin', to: 'users#create'
+  end
+
   devise_scope :user do
     get 'users/update_departments', to: 'users/registrations#update_departments'
     get 'users/update_laboratories', to: 'users/registrations#update_laboratories'
