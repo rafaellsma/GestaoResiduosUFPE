f882e1b70101673949e355fe7ad9676be13d78e3
Bruno Filho <brgccf@cin.ufpe.br>
2017-03-17 17:41:57 -0300

    Correção de ajuste em sedimentos - Primeiro passo com controlador de User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def create
-
+    @user = User.new(user_params)
+    
+  end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :phone_ext)
   end
 end
