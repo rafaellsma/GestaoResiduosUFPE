25283662e67d1db5702684ee32dc28bd577b4289
Bruno Filho <brgccf@cin.ufpe.br>
2017-03-31 10:55:42 -0300

    Ajustando Testes para fazer select com numerais

--- features/step_definitions/create_sediments_steps.rb
+++ features/step_definitions/create_sediments_steps.rb
@@ -35,6 +35,10 @@ When(/^I select "([^"]*)" with "([^"]*)" option$/) do |field, option|
   find("##{field}").find(:xpath, "option[#{option}]").select_option
 end
 
+When(/^I select "([^"]*)" with (\d+) option$/) do |field, option|
+  find("##{field}").find(:xpath, "option[#{option}]").select_option
+end
+
 When(/^I click "([^"]*)"$/) do |msg|
   click_button msg
   p Sediment.all
