7e2bd3a51176b4f513c40eabc90df6e91213d1f2
Bruno Filho <brgccf@cin.ufpe.br>
2017-05-01 21:43:19 -0300

    Finalizando refatoração de Cadastro de Resíduos: - Corrigiu falha no controlador para capturar volume - Corrigiu falha no controlador que capturava a mesma informação duas vezes. Agora captura o nome do lab em local - Atualização das Views para informar volume do resíduo corretamente

--- app/controllers/sediments_controller.rb
+++ app/controllers/sediments_controller.rb
@@ -28,7 +28,7 @@ class SedimentsController < ApplicationController
     @sediment.data_registered = Time.now
     @sediment.laboratory = current_user.laboratory
     @sediment.user = current_user
-    @sediment.local = current_user.laboratory
+    @sediment.local = current_user.laboratory.name
     respond_to do |format|
       if @sediment.save
         format.html { redirect_to @sediment, notice: 'Residuo foi criado com sucesso.' }
@@ -72,6 +72,6 @@ class SedimentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sediment_params
-      params.require(:sediment).permit(:composition, :weight, :local, :data_created,:res_type ,:data_registered)
+      params.require(:sediment).permit(:composition, :weight, :local, :res_type, :volume ,:data_registered)
     end
 end
