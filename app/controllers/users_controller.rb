7e2d360c937e6a3230252ddabd751259dedabeea
Bruno Filho <brgccf@cin.ufpe.br>
2017-05-26 14:16:29 -0300

    Refatorando implementação e testes para cadastro de administradores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,26 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :phone_ext)
   end
+  def index
+    @users = []
+    @users = User.where(approved: true, admin: false) if params[:type] == '1'
+    @users = User.where(approved: false, admin: false) if params[:type] == '0'
+    flash[:notice] = ''
+    if !params[:type].blank? && @users.empty?
+      flash[:notice] = "Não existe nenhum usuário"
+    end
+  end
+
+  def approve
+    user = User.find(params[:user_id])
+    @users = User.where(approved: false, admin: false)
+    if params[:commit] == 'desaprove!'
+      user.disapprove!
+      redirect_to controller: 'users', action: 'index', type: 1
+    else
+      user.approve!
+      redirect_to controller: 'users', action: 'index', type: 0
+    end
+
+  end
 end
--- features/step_definitions/create_user_steps.rb
+++ features/step_definitions/create_user_steps.rb
@@ -27,7 +27,7 @@ When(/^I select option "([^"]*)" in "([^"]*)"$/) do |option, sel|
 end
 
 When(/^I submit form$/) do
-  click_button 'Sign up'
+  click_button 'Cadastrar'
 end
 
 Then(/^User with email "([^"]*)", name "([^"]*)", phone_ext ([0-9]+), password "([^"]*)" and laboratory "([^"]*)" was created$/) do |email, name, phone_ext, password, laboratory|
@@ -78,7 +78,7 @@ end
 
 
 And(/^I make login$/) do
-  click_button 'Log in'
+  click_button 'Entrar'
 end
 
 And(/^I submit new admin user form$/) do
