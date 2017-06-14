c99b82805c2799825852de5b8b10f9f132d453e6
Rafael Albuquerque <rafaellsma@gmail.com>
2017-06-13 22:06:22 -0300

    Ajeita os testes que quebraram

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    redirect_to controller:'devise/sessions', action: 'new' unless current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,20 @@ Rails.application.routes.draw do
   }
   authenticate :user do
     resources :sediments
-    root to: "home#index"
   end
+
   resources :laboratories
   resources :departments
   resources :centers
 
-
+  root to: 'home#index'
+  
   authenticated :user, lambda { |u| u.admin? } do
     get 'list', to: 'users#index'
     post 'approve', to: 'users#approve'
   end
 
   devise_scope :user do
-
     get 'users/update_departments', to: 'users/registrations#update_departments'
     get 'users/update_laboratories', to: 'users/registrations#update_laboratories'
   end
