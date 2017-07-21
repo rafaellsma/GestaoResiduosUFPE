d123f774d7920978df9efb8553d2b9162c3f6af5
Rafael Albuquerque <rafaellsma@gmail.com>
2017-07-21 17:13:43 -0300

    Fix js to dinamically dropdown

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,31 +1,32 @@
-$(document).on('change', '#centers_select', function(evt) {
-  $.ajax('update_departments', {
-    type: 'GET',
-    dataType: 'script',
-    data: {
-      center_id: $("#centers_select option:selected").val()
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      return console.log("AJAX Error: " + textStatus);
-    },
-    success: function(data, textStatus, jqXHR) {
-      return console.log("OK!");
-    }
+$(document).on('turbolinks:load', function() {
+  $("#centers_select").change(function(){
+    $.ajax('update_departments', {
+      type: 'GET',
+      dataType: 'script',
+      data: {
+        center_id: $("#centers_select option:selected").val()
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        return console.log("AJAX Error: " + textStatus);
+      },
+      success: function(data, textStatus, jqXHR) {
+        return console.log("OK!");
+      }
+    });
   });
-});
-
-$(document).on('change', '#departments_select', function(evt) {
-  $.ajax('update_laboratories', {
-    type: 'GET',
-    dataType: 'script',
-    data: {
-      department_id: $("#departments_select option:selected").val()
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      return console.log("AJAX Error: " + textStatus);
-    },
-    success: function(data, textStatus, jqXHR) {
-      return console.log("OK!");
-    }
+  $("#departments_select").change(function(){
+    $.ajax('update_laboratories', {
+      type: 'GET',
+      dataType: 'script',
+      data: {
+        department_id: $("#departments_select option:selected").val()
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        return console.log("AJAX Error: " + textStatus);
+      },
+      success: function(data, textStatus, jqXHR) {
+        return console.log("OK!");
+      }
+    });
   });
 });
