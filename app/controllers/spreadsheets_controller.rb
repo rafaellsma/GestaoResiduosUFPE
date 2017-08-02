52f95733131ead8502c964dbdb19fa699ebc2137
Rafael Albuquerque <rafaellsma@gmail.com>
2017-08-02 08:24:41 -0300

    split center, department and laboratory in spreadsheet

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -19,21 +19,34 @@ class SpreadsheetsController < ApplicationController
     sheet = book.create_worksheet
     i = 0
     Sediment.attribute_names.each do |name|
-      sheet[0,i] = t("content.sediments.index.#{name}");
-      i += 1
+      if name == 'laboratory_id'
+        sheet[0,i] = 'Centro'
+        sheet[0,i+1] = 'Departamento'
+        sheet[0,i+2] = t("content.sediments.index.#{name}")
+        i += 3
+      else
+        sheet[0,i] = t("content.sediments.index.#{name}")
+        i += 1
+      end
     end
     sediments.each_with_index do |sediment, row|
-      sediment.attribute_names.each_with_index do |name, column|
+      column = 0
+      sediment.attribute_names.each_with_index do |name|
         if (name == 'laboratory_id')
           laboratory = sediment.laboratory
           department = laboratory.department
-          sheet[row+1, column] = "#{laboratory.name}/#{department.name}/#{department.center.name}"
+          sheet[row+1, column] = department.center.name
+          sheet[row+1, column+1] = department.name
+          sheet[row+1, column+2] = laboratory.name
+          column += 3
+          next
         elsif (name == 'user_id')
           user = sediment.user
           sheet[row+1, column] = "Nome: #{user.name} Email: #{user.email} Ramal: #{user.phone_ext}"
         else
           sheet[row+1, column] = sediment.send(name)
         end
+        column += 1
       end
     end
     sheet = autofit(sheet)
@@ -44,7 +57,7 @@ class SpreadsheetsController < ApplicationController
   end
 
   def autofit(worksheet)
-    (0...worksheet.column_count).each do |col_idx| 
+    (0...worksheet.column_count).each do |col_idx|
       column = worksheet.column(col_idx)
       column.width = column.each_with_index.map do |cell, row|
         chars = cell.present? ? cell.to_s.strip.split('').count + 3 : 1
