91000674b696bca777233ad3fb84d10a847fa892
rlsma <rafaellsma@gmail.com>
2017-09-03 14:24:51 -0300

    disable turbolinks from link_to

--- app/assets/javascripts/laboratories.js
+++ app/assets/javascripts/laboratories.js
@@ -7,22 +7,5 @@ ready = function() {
 	});
 };
 
-var ready2;
-ready2 = function() {
-	console.log("ready2")
-};
-var ready3;
-ready2 = function() {
-	console.log("ready3")
-};
-var ready4;
-ready2 = function() {
-	console.log("ready4")
-};
-
 $(document).ready(ready);
 $(document).on('page:load', ready);
-
-$(document).ready(ready3);
-$(document).on('page:load', ready3);
-$(document).on('turbolinks:load', ready4);
