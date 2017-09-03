c3eacbf19d18864a7ae352c0064fc9713fcdf465
rlsma <rafaellsma@gmail.com>
2017-09-03 13:28:50 -0300

    fix to heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '92265eef09005e18a335588fc21b5ae48c819047f06c36401d47b384f8cc8e6e5ba1ad50201f248b84c87ffe15dcc48cf98f9c5a9850aa227a66c32cb3e00825'
+  config.secret_key = '92265eef09005e18a335588fc21b5ae48c819047f06c36401d47b384f8cc8e6e5ba1ad50201f248b84c87ffe15dcc48cf98f9c5a9850aa227a66c32cb3e00825'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
