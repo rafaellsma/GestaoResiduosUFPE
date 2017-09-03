f20fefdf6a9ca161facbcbb0e34a8c559dff5545
rlsma <rafaellsma@gmail.com>
2017-09-03 14:10:41 -0300

    turbolinks problems

--- app/assets/javascripts/laboratories.js
+++ app/assets/javascripts/laboratories.js
@@ -1,23 +1,12 @@
-$(document).on('turbolinks:load', function() {
-  $('.chosen-select').chosen({
+var ready;
+ready = function() {
+	$('.chosen-select').chosen({
 	  allow_single_deselect: true,
 	  no_results_text: 'Nenhum resultado encontrado',
 	  width: '100%'
 	});
-})
+};
 
-$(document).on('ready', function() {
-  $('.chosen-select').chosen({
-	  allow_single_deselect: true,
-	  no_results_text: 'Nenhum resultado encontrado',
-	  width: '100%'
-	});
-})
-
-$(document).on('page:change', function() {
-  $('.chosen-select').chosen({
-	  allow_single_deselect: true,
-	  no_results_text: 'Nenhum resultado encontrado',
-	  width: '100%'
-	});
-})
+$(document).ready(ready);
+$(document).on('page:load', ready);
+$(document).on('turbolinks:load', ready);
