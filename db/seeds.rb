a5762d732cb2b3e1323711002897fe1cfa1ea91c
Rafael Albuquerque <rafaellsma@gmail.com>
2017-11-05 14:52:40 -0300

    Change xlsx to xls

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# c = Center.create(name: 'Centro de informática')
+# d = Department.create(name: 'Departamento de Informação e Sistemas', center: c)
+# l = Laboratory.create(name: 'Laboratório de Hardware', department: d)
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-c = Center.create(name: 'Centro de informática')
-d = Department.create(name: 'Departamento de Informação e Sistemas', center: c)
-l = Laboratory.create(name: 'Laboratório de Hardware', department: d)
-
-User.create(name: 'admin', email: "admin@admin.com", password: "admin123", phone_ext: 123, admin: true, approved: true)
+# User.create(name: 'admin', email: "admin@admin.com", password: "admin123", phone_ext: 123, admin: true, approved: true)
+book = Spreadsheet.open(Rails.root.to_s+'/db/labs.xls')
+p book
